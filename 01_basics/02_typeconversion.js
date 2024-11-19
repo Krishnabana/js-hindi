e3080bfaf397579d5c85f1e302d75bd0c70450b2
Krishnabana <127650655+Krishnabana@users.noreply.github.com>
2024-11-19 18:07:47 +0000

    conversion of data types

---
+++ 01_basics/02_typeconversion.js
@@ -0,0 +1,16 @@
+let score = "33fc"
+let sc = Number(score)
+
+// we can only convert the type of data but can't convert its actually valye in this case
+console.log(typeof sc);
+console.log(sc);
+
+/* some of examples are:- 
+33 => 33
+"33fc" => NaN (not a number)
+"true" => 1 or false => 0 and also 1 as true and 0 as false when we convert num to bool
+"" (empty string) => false
+and if we convert number to string that it is converted easily
+33 => 33 (but its type is string)
+*/
+
